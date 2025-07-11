2b41688a5c7ddc17e0a5d7671eecdaa0143042fb
Silas <chibukesilas@gmail.com>
2025-07-11 19:52:47 +0100

    Fixed Bugs

--- src/components/sections/Corner.tsx
+++ src/components/sections/Corner.tsx
@@ -11,6 +11,7 @@ export const Navlinks = [
   { label: "Home", href: "/" },
   { label: "About", href: "/about" },
   { label: "Services", href: "/services" },
+  { label: "Properties", href: "/properties" },
   { label: "Blog", href: "/blog" },
   { label: "Contact", href: "/contact" },
 ];
